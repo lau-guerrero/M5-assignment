3c4763ac76b899a1515f19bc8fa60fb805660f78
lau-guerrero <laura.guerrero.gm@gmail.com>
2025-10-22 01:43:51 -0700

    step1

---
+++ js/01-functions-practice.js
@@ -0,0 +1,16 @@
+//STEP 1
+function halfNumber(num) {
+  const result = num / 2;
+  console.log(`Half of ${num} is ${result}.`);
+  return result;
+}
+
+halfNumber(5);
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
--- js/script.js
+++
@@ -1,5 +0,0 @@
-function halfNumber(num) {
-  const result = num / 2;
-  console.log(`Half of ${num} is ${result}.`);
-  return result;
-}
